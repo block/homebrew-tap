6355c777c53c7a7897ae0ae7736f45757a070043
Tom Mulcahy <tomm@squareup.com>
2025-06-02 11:33:33 -0700

    depend on rsync
    Stoic needs modern rsync - the one shipped with Mac is too far out of
    date.

--- Formula/stoic.rb
+++ Formula/stoic.rb
@@ -5,6 +5,8 @@ class Stoic < Formula
   sha256 "2cf5ce3d2b0d99098232f2fc364fbcc491b2401ef98ddbd4218eab50a8fea6aa"
   license "Apache-2.0"
 
+  depends_on "rsync"  # Ensure modern rsync is available
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/stoic"
