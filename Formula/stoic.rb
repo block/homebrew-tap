9ab2e5f8f6297b9e59b8884648b711faec87ca9e
Tom Mulcahy <tomm@squareup.com>
2025-06-03 06:47:09 -0700

    Publish stoic v0.0.5

--- Formula/stoic.rb
+++ Formula/stoic.rb
@@ -1,8 +1,8 @@
 class Stoic < Formula
   desc "CLI tool for Android investigations"
   homepage "https://github.com/block/stoic"
-  url "https://github.com/block/stoic/releases/download/v0.0.4/stoic-0.0.4.tar.gz"
-  sha256 "2cf5ce3d2b0d99098232f2fc364fbcc491b2401ef98ddbd4218eab50a8fea6aa"
+  url "https://github.com/block/stoic/releases/download/v0.0.5/stoic-0.0.5.tar.gz"
+  sha256 "f1e67b957cb20ed7ab0433c788f9df523b8b9ba9cd9790fbdc090355a21c5f30"
   license "Apache-2.0"
 
   depends_on "rsync"  # Ensure modern rsync is available
